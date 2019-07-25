fb5f23d4102080f0bb55436df472019611ad7026
Andrea Tongsak <atongsak@gmail.com>
2019-07-25 14:17:34 -0700

    Initial Commit

---
+++ NaviWalk/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NaviWalk
+//
+//  Created by Andrea Tongsak on 7/25/19.
+//  Copyright © 2019 Andrea Tongsak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
